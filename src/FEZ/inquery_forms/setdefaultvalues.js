f3e4e02f0bc5db3e97ea6e0f554844fe6c96405b
umyuu <124dtiaka@gmail.com>
2017-04-22 04:46:08 +0900

    remove Library jquery

--- src/FEZ/inquery_forms/setdefaultvalues.js
+++ src/FEZ/inquery_forms/setdefaultvalues.js
@@ -3,12 +3,12 @@ function setValue(key, value) {
 	if (element == null) return;
 	element.value = value;
 };
-$(() => {
-	var now = new Date();
+function setdefaultvalues() {
+	const now = new Date();
 	//---------------------------------------------------------------
 	// 【不具合のご報告】 フォーム
 	//---------------------------------------------------------------
-    setValue('select[name="trouble_date1"]', now.getFullYear());
+	setValue('select[name="trouble_date1"]', now.getFullYear());
 	setValue('select[name="trouble_date2"]', now.getMonth() + 1);
 	setValue('select[name="trouble_date3"]', now.getDate());
 	setValue('select[name="trouble_date4"]', now.getHours());
@@ -16,7 +16,7 @@ $(() => {
 	//---------------------------------------------------------------
 	// 【迷惑行為のご報告】 フォーム
 	//---------------------------------------------------------------
-    setValue('select[name="lova_report_datetime1"]', now.getFullYear());
+	setValue('select[name="lova_report_datetime1"]', now.getFullYear());
 	setValue('select[name="lova_report_datetime2"]', now.getMonth() + 1);
 	setValue('select[name="lova_report_datetime3"]', now.getDate());
 	setValue('select[name="lova_report_datetime4"]', now.getHours());
@@ -24,4 +24,6 @@ $(() => {
 	// 【不具合のご報告】、 【迷惑行為のご報告】フォームの両方にある項目
 	//---------------------------------------------------------------
 	setValue('select[name="fez_world"]', 'ワールド統合後');
-});
+};
+// call function 
+setdefaultvalues();
