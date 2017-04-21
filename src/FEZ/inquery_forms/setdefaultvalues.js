eb0170c661c93171e62da75ace667d1441f5c063
umyuu <124dtiaka@gmail.com>
2017-04-22 04:02:55 +0900

    add FEZ\inquery_forms

---
+++ src/FEZ/inquery_forms/setdefaultvalues.js
@@ -0,0 +1,27 @@
+function setValue(key, value) {
+	const element = document.querySelector(key);
+	if (element == null) return;
+	element.value = value;
+};
+$(function() {
+	var now = new Date();
+	//---------------------------------------------------------------
+	// 【不具合のご報告】 フォーム
+	//---------------------------------------------------------------
+    setValue('select[name="trouble_date1"]', now.getFullYear());
+	setValue('select[name="trouble_date2"]', now.getMonth() + 1);
+	setValue('select[name="trouble_date3"]', now.getDate());
+	setValue('select[name="trouble_date4"]', now.getHours());
+	setValue('select[name="webbrowser_name"]', 'Google Chrome');
+	//---------------------------------------------------------------
+	// 【迷惑行為のご報告】 フォーム
+	//---------------------------------------------------------------
+    setValue('select[name="lova_report_datetime1"]', now.getFullYear());
+	setValue('select[name="lova_report_datetime2"]', now.getMonth() + 1);
+	setValue('select[name="lova_report_datetime3"]', now.getDate());
+	setValue('select[name="lova_report_datetime4"]', now.getHours());
+	//---------------------------------------------------------------
+	// 【不具合のご報告】、 【迷惑行為のご報告】フォームの両方にある項目
+	//---------------------------------------------------------------
+	setValue('select[name="fez_world"]', 'ワールド統合後');
+});
