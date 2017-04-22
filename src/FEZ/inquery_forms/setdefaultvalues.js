050dec684ed8aaaf1f441d3233924b9a2df59b7a
umyuu <124dtiaka@gmail.com>
2017-04-22 14:52:20 +0900

    COUNTRIES selectbox

--- src/FEZ/inquery_forms/setdefaultvalues.js
+++ src/FEZ/inquery_forms/setdefaultvalues.js
@@ -1,8 +1,41 @@
+const COUNTRIES = [
+	{ text:'', value:'' },
+    { text:'ネツァワル', value:'ネツァワル' },
+    { text:'カセドリア', value:'カセドリア' },
+    { text:'エルソード', value:'エルソード' },
+	{ text:'ホルデイン', value:'ホルデイン' },
+	{ text:'ゲブランド', value:'ゲブランド' },
+];
+// セレクトボックスを作成
+function createSelectBox(element, options){
+    element.length = options.length;
+    for (var i = 0; i < options.length; i++){
+		var item = options[i];
+        element.options[i].text = item.text;
+        element.options[i].value = item.value;
+    }
+};
+// 値を設定
 function setValue(key, value) {
 	const element = document.querySelector(key);
 	if (element == null) return;
 	element.value = value;
 };
+// 国家名のセレクトボックスを作成
+// セレクトボックスを選択時に、input項目に反映
+function createCountries(key) {
+	const element = document.querySelector(key);
+	if (element == null) return;
+	const select = document.createElement('select');
+	createSelectBox(select, COUNTRIES);
+	// セレクトボックス => input項目への反映
+	select.onchange = (event) => {
+		const target = event.target;
+		element.value = target.options[target.selectedIndex].value;
+	};
+	element.parentNode.insertBefore(select, element.nextSibling);
+};
+
 function setdefaultvalues() {
 	const now = new Date();
 	//---------------------------------------------------------------
@@ -20,6 +53,8 @@ function setdefaultvalues() {
 	setValue('select[name="lova_report_datetime2"]', now.getMonth() + 1);
 	setValue('select[name="lova_report_datetime3"]', now.getDate());
 	setValue('select[name="lova_report_datetime4"]', now.getHours());
+	createCountries('input[name="fez_reporter_region"]'); //通報者の国家名
+	createCountries('input[name="fez_report_region"]'); //違反者の国家名
 	//---------------------------------------------------------------
 	// 【不具合のご報告】、 【迷惑行為のご報告】フォームの両方にある項目
 	//---------------------------------------------------------------
