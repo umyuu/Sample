f123c79ad23a4eba719df55ba41042dce769a432
umyuu <124dtiaka@gmail.com>
2017-12-11 02:29:31 +0900

    Q104138

---
+++ src/Python3/Q104138/src/Q104138.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class Q104138 {
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		final String search_key = "2017-11-30";
+		final URL url = Q104138.class.getClassLoader().getResource("resources/indices_I101.csv");
+
+		try (BufferedReader br = Files.newBufferedReader(Paths.get(url.toURI()), Charset.forName("Windows-31j"))) {
+			try (Stream<String> lines = br.lines()) {
+				lines.filter(line -> line.startsWith(search_key)).forEach(line -> {
+					System.out.println(line);
+				});
+			}
+		}
+	}
+}
