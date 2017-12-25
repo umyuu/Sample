d9da442d2acc358f12a7f18728c218a47368083a
umyuu <124dtiaka@gmail.com>
2017-12-25 10:25:15 +0900

    use java.time.LocalDate;

--- src/Java/Q105888/Q105888.java
+++ src/Java/Q105888/Q105888.java
@@ -1,59 +1,39 @@
-import java.util.List;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.io.*;
+import java.util.List;
+import java.util.Scanner;
 
 class ToDo {
 	int number;
-	int month;
-	int day;
-	String item;
+	final LocalDate localdate;
+	final String item;
 	int priority;
+
+	ToDo(String item, LocalDate localdate) {
+		this.localdate = localdate;
+		this.item = item;
+		this.priority = 0;
+	}
 
 	@Override
 	public String toString() {
-		return this.month + "月" + this.day + "日 " + this.item + "優先度:" + this.priority;
+		return this.localdate.format(DateTimeFormatter.ofPattern("MM月dd日")) + " " + this.item + "優先度:" + this.priority;
 	}
 }
 
 public class Q105888 {
 	public static void main(String[] args) throws IOException {
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-			//配列から動的配列(ArrayList)に宣言を変更。
-			List<ToDo> ToDoList = new ArrayList<>();
-
+		// 配列から動的配列(ArrayList)に宣言を変更。
+		List<ToDo> ToDoList = new ArrayList<>();
+		try (Scanner sc = new Scanner(System.in)) {
 			while (true) {
-				System.out.println("予定を追加するなら1を入力。予定を月順で表示するなら２、優先度順で表示するなら３を入力。");
-
-				String str = br.readLine();
-				int num = Integer.parseInt(str);
+				String str = Prompt(sc, "予定を追加するなら1を入力。予定を月順で表示するなら２、優先度順で表示するなら３を入力。");
+				final int num = Integer.parseInt(str);
 				switch (num) {
 				case 1:
-					ToDo todo = new ToDo();
-					do {
-						System.out.println("月を入力して下さい(1〜12)");
-						String month = br.readLine();
-						todo.month = Integer.parseInt(month);
-					} while (!(1 <= todo.month && todo.month <= 12));
-					// ↑1〜12以外が入力されたら再入力を求める
-					
-					do {
-						System.out.println("日を入力して下さい(1〜31)");
-						String day = br.readLine();
-						todo.day = Integer.parseInt(day);
-					} while (32 <= todo.day);
-
-					System.out.println("予定");
-					String item = br.readLine();
-					todo.item = item;
-
-					do {
-						System.out.println("優先度を入力して下さい(1〜5)");
-						String priority = br.readLine();
-						todo.priority = Integer.parseInt(priority);
-					} while (6 <= todo.priority);
-					
-					ToDoList.add(todo);
+					add_todo(sc, ToDoList);
 					break;
 				case 2:
 					display_month_order(ToDoList);
@@ -66,26 +46,49 @@ public class Q105888 {
 		}
 	}
 
+	private static void add_todo(Scanner sc, List<ToDo> ToDoList) {
+
+		int month;
+		do {
+			String input_month = Prompt(sc, "月を入力して下さい(1〜12)");
+			month = Integer.parseInt(input_month);
+		} while (!(1 <= month && month <= 12));
+		// ↑1〜12以外が入力されたら再入力を求める
+		int dayOfMonth;
+		do {
+			String input_day = Prompt(sc, "日を入力して下さい(1〜31)");
+			dayOfMonth = Integer.parseInt(input_day);
+		} while (!(1 <= dayOfMonth && dayOfMonth <= 31));
+		
+		String input_item = Prompt(sc, "予定");
+		LocalDate localdate = LocalDate.of(LocalDate.now().getYear(), month, dayOfMonth);
+		ToDo todo = new ToDo(input_item, localdate);
+		do {
+			String input_priority = Prompt(sc, "優先度を入力して下さい(1〜5)");
+			todo.priority = Integer.parseInt(input_priority);
+		} while (!(1 <= todo.priority && todo.priority <= 5));
+
+		ToDoList.add(todo);
+	}
+
+	private static String Prompt(Scanner sc, String msg) {
+		System.out.println(msg);
+		return sc.nextLine();
+	}
+
 	private static void display_month_order(List<ToDo> ToDoList) {
 		System.out.println("予定です。(月昇順)");
-		ToDoList.sort(new Comparator<ToDo>() {
-			@Override
-			public int compare(ToDo todo1, ToDo todo2) {
-				// 標準ライブラリのInteger.compareを使用すると、compareをシンプルに記述することができます。
-				int month = Integer.compare(todo1.month, todo2.month);
-				// ガード節による入れ子条件記述の置き換え
-				if (month != 0) {
-					return month;
-				}
-				return Integer.compare(todo1.day, todo2.day);
-			}
+		ToDoList.sort((a, b) -> {
+			return a.localdate.compareTo(b.localdate);
 		});
 		ToDoList.forEach(t -> System.out.println(t));
 	}
 
 	private static void display_priority_order(List<ToDo> ToDoList) {
 		System.out.println("予定です。(優先度昇順)");
-		ToDoList.sort((a, b) -> a.priority - b.priority);
+		ToDoList.sort((a, b) -> {
+			return Integer.compare(a.priority, b.priority);
+		});
 		ToDoList.forEach(t -> System.out.println(t));
 	}
 }
