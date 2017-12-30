b8b88c8bf56b1e8fbc7b200a56621f2a4d40f13c
umyuu <124dtiaka@gmail.com>
2017-12-30 17:56:07 +0900

    Q107020

---
+++ src/Java/Q107020/Recur3.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Recur3 {
+	static void recur3(int n) {
+		if (n <= 0) {
+			throw new UnsupportedOperationException(String.valueOf(n));
+		}
+		// new int[100]で宣言する意味はないです。
+		int[] nstk = new int[n];
+		int[] sstk = new int[n];
+		int ptr = -1;
+		int sw = 0;
+
+		while (true) {
+			if (n > 0) {
+				ptr++;
+				nstk[ptr] = n;
+				sstk[ptr] = sw;
+				if (sw == 0)
+					n -= 1;// n = n - 1;
+				else if (sw == 1) {
+					n -= 2;
+					sw = 0;
+				}
+				continue;
+			}
+			do {
+				n = nstk[ptr];
+				sw = sstk[ptr] + 1;
+				// 動作確認のために変数ptrと配列の内容をデバックプリント
+				System.out.println(Stream.generate(() -> "-").limit(30).collect(Collectors.joining("")));
+				System.out.println(ptr);
+				System.out.println(Arrays.toString(nstk) + ' ' + Arrays.toString(sstk));
+				ptr--;
+				if (sw == 2) {
+					System.out.println(n);
+					if (ptr < 0)
+						return;
+				}
+			} while (sw == 2);
+		}
+	}
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("整数を入力せよ：");
+			int x = Integer.parseInt(sc.nextLine());
+			recur3(x);
+		}
+	}
+}
