5e44c571d2e9d25f66cf8b5efb31ad01c58099a2
umyuu <124dtiaka@gmail.com>
2018-01-06 23:01:06 +0900

    Q107683

--- src/Java/Q107683/GameOver.java
+++ src/Java/Q107683/GameOver.java
@@ -16,11 +16,14 @@ public class GameOver extends JPanel {
 		setLayout(new BorderLayout(0, 0));
 		// drawStringはコンポーネントの領域を計算する必要があるので、文字列を表示するだけならJLabelの方がいいです。
 		JLabel lblGameOver = new JLabel("GameOver");
+		// 文字色を赤色にする。
+		lblGameOver.setForeground(Color.RED);
 		lblGameOver.setVerticalAlignment(SwingConstants.TOP);
 		lblGameOver.setHorizontalAlignment(SwingConstants.CENTER);
 		add(lblGameOver, BorderLayout.NORTH);
 		
 		JLabel lblScore = new JLabel("Score");
+		lblScore.setForeground(Color.RED);
 		lblScore.setHorizontalAlignment(SwingConstants.CENTER);
 		add(lblScore);
 
