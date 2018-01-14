4ff47e5845d3f271c96dc1e1dfea0aee0b6d19c9
umyuu <124dtiaka@gmail.com>
2018-01-14 18:55:13 +0900

    Q108601

---
+++ src/Python3/Q108601/Web/cgi-bin/kakugen.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print("Content-Type: text/html\n")
+
+print('<!DOCTYPE html><html lang="ja"><head><title></title></head><body>')
+print("聞くことに速く語ることに遅くあるべき")
+print('</body></html>')
