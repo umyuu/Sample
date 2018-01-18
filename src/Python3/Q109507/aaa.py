a2c2408aad4b309416ef051dfad42c97e0395fb7
umyuu <124dtiaka@gmail.com>
2018-01-18 22:41:04 +0900

    Q109507

---
+++ src/Python3/Q109507/aaa.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*
+print(__name__)
+print('aaa.py caller')
+
+
+def calculation(n):
+    print('calculation')
+    return 2**n
---
+++ src/Python3/Q109507/bbb.py
@@ -0,0 +1,6 @@
+# -*- coding: UTF-8 -*
+import aaa
+print(__name__)
+print('bbb.py caller')
+
+print(aaa.calculation(2))
