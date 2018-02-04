1b5a03755a4cd782185908793571cd563c42123f
umyuu <124dtiaka@gmail.com>
2018-02-04 18:28:09 +0900

    Q111938

---
+++ src/Python2/Q111938/exsample.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8 -*-
+from __future__ import unicode_literals
+import sys
+print(sys.version)
+
+def main():
+    print('234'.isdecimal())
+
+
+if __name__ == '__main__':
+    main()
