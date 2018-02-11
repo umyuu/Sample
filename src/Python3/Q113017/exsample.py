e44d5eadc7319877cf18e7e2658d69aada448cd1
umyuu <124dtiaka@gmail.com>
2018-02-12 02:03:40 +0900

    A113017

--- src/Python3/Q113017/exsample.py
+++ src/Python3/Q113017/exsample.py
@@ -2,12 +2,29 @@
 import linecache
 
 
+def split_word_list(line: str) -> list:
+    word_list = []
+    word = []
+    for c in line:
+        if c == ' ':
+            print(word)
+            word_list.append(word)
+            word = []
+            continue
+        word.append(c)
+    else:
+        print(word)
+        word_list.append(word)
+    return word_list
+
+
 def main() -> None:
     line_no = 1
     file_name = r'sample.txt'
     target_line = linecache.getline(file_name, line_no)
-    for c in target_line:
-        print(c)
+    target_line = 'This is an apple'
+    word_list = split_word_list(target_line)
+    print(word_list)
 
 
 if __name__ == '__main__':
