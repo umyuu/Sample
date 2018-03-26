fcc8f791a31fa93e192b2baffeaadd7c8c0193e1
umyuu <124dtiaka@gmail.com>
2018-03-26 09:46:58 +0900

    A118609

--- src/Python3/A118609/exsample.py
+++ src/Python3/A118609/exsample.py
@@ -1,5 +1,6 @@
-import json
+import time
 from datetime import datetime
+import json
 import requests
 from urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
@@ -13,21 +14,39 @@ def get_content(url: str='https://api.cryptowat.ch/markets/bitflyer/btcfxjpy/ohl
     s.mount('https://', HTTPAdapter(max_retries=retries))
     payload = {'periods': '60'}
     r = s.get(url, params=payload)
-    return r.json()['result']['60']
+    # ↓テスト用コード
+    # r.status_code = 429
+    if r.status_code == 429:
+        # APIのドキュメントにはr.json()できるかどうかの保証の記述はなかったので注意です。
+        # r.json()出来ない場合は値:Noneを返してくださいな。
+        return r.json(), r.status_code
+    return r.json(), r.status_code
 
 
 def data_write(obj):
     file_name = f'{datetime.now().strftime("%Y%m%d_%H%M%S_%f")}.json'
-    print(f'file write:{file_name}')
+    #print(f'file write:{file_name}')
     with open(file_name, 'w', encoding='utf-8-sig') as f:
         json.dump(obj, f)
 
 
 def main() -> None:
-    content = get_content()
-    print(content)
-    # file作成
-    data_write(content)
+    CLOSE = 4
+    while True:
+        content, status_code = get_content()
+        # rate_limitなら
+        if status_code == 429:
+            # sleepタイムを55秒に
+            time.sleep(55)
+            continue
+
+        json = content['result']['60']
+        close = json[-1][CLOSE]
+        print(str(datetime.fromtimestamp(round(time.time()) + 60 * 60 * 9)) + ' CLOSE: ' + str(close))  # 直近の終値の表示
+        # ファイルに保存したい時。
+        #data_write(json)
+
+        time.sleep(5)
 
 
 if __name__ == '__main__':
