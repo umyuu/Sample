ee4ad296244798bfc83b7388117a75d51dd02815
umyuu <124dtiaka@gmail.com>
2018-04-14 20:49:56 +0900

    A121776
    accept ip ip_address#is_loopback,ip_address#is_loopback

--- src/Python3/A121776/exsample.py
+++ src/Python3/A121776/exsample.py
@@ -2,20 +2,31 @@
 import os
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from socketserver import ThreadingMixIn
+from ipaddress import ip_address
 import html
 
 
 class MyHandler(SimpleHTTPRequestHandler):
-    def to_content(self) ->str:
+    def to_content(self) -> str:
         body = f"time:{html.escape(self.date_time_string())}<br>dir:{html.escape(os.getcwd())}"
         content = f"<html><head></head><body>{body}</body></html>"
         return content
 
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(self.to_content().encode('utf-8'))
-        self.wfile.write(b'\n')
+        req_ip = ip_address(self.client_address[0])
+        # IPアドレスによる簡易的なアクセス制限
+        # ループバック,ローカルアドレス以外はステータスコード:403を返す
+        is_accepted = any([req_ip.is_loopback, req_ip.is_private])
+        if is_accepted:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(self.to_content().encode('utf-8'))
+            self.wfile.write(b'\n')
+            return
+        else:
+            self.send_response(403)
+            self.end_headers()
+            return
 
 
 class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
@@ -26,7 +37,7 @@ def main() ->None:
     PORT = 8000
     print(os.getcwd())
     print(os.path.abspath(__file__))
-    with  ThreadingHTTPServer(("", PORT), MyHandler) as httpd:
+    with ThreadingHTTPServer(("", PORT), MyHandler) as httpd:
         print("serving at port", PORT)
         httpd.serve_forever()
 
